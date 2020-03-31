febe7f9a701fe25193bbca2c970dac77dcefea59
William Lew <WilliamMilesLew@gmail.com>
2020-03-31 16:55:33 -0700

    added: file parsing for new language

--- src/parser.cpp
+++ src/parser.cpp
@@ -2,16 +2,58 @@
 #include <stdio.h>
 
 Parser::Parser() {
-
+  file = new std::fstream;
+  currentToken = new std::string;
 }
 
 Parser::~Parser() {
+  delete currentToken;
 
+  file->close();
+  delete file;
 }
 
 Scene* Parser::createSceneFromFile(const char* filename) {
   Scene* scene = new Scene();
+  file->open(filename);
+  RST* rootTree;
 
-  printf("%s\n", filename);
+  rootTree = ParseWorld();
+
   return nullptr;
+}
+
+bool Parser::nextToken() {
+  if (*file >> *currentToken) {
+    return true;
+  }
+
+  return false;
+}
+
+void Parser::expectToken(std::string token) {
+  if (*currentToken != token) {
+    throw;
+  }
+}
+
+void Parser::expectIdentifier() {
+  int x = 0;
+  while (x < currentToken->length()) {
+    if (!std::isalnum((*currentToken)[x])) {
+      throw;
+    }
+    x++;
+  }
+}
+
+RST* Parser::ParseWorld() {
+  RST* tree = new WorldRST;
+  
+  nextToken();
+  expectToken("World");
+  nextToken();
+  expectIdentifier();
+
+  return tree;
 }
--- src/parser.h
+++ src/parser.h
@@ -1,8 +1,26 @@
 #pragma once
+#include <fstream>
+#include <string>
 #include "scene.h"
+
+struct RST {
+  std::vector<RST*> childrenList;
+};
+
+struct WorldRST : RST {
+  std::string identifier;
+};
 
 class Parser {
 private:
+  std::fstream* file;
+  std::string* currentToken;
+
+  bool nextToken();
+  void expectToken(std::string token);
+  void expectIdentifier();
+
+  RST* ParseWorld();
 public:
   Parser();
   ~Parser();
