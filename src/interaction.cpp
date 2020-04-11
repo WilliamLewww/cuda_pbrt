2a6b41fdcf03ea72546af6dabc1780013824f931
William Lew <WilliamMilesLew@gmail.com>
2020-04-11 14:30:18 -0700

    added: normal calculation in surface interaction

--- src/interaction.cpp
+++ src/interaction.cpp
@@ -6,6 +6,8 @@ bool Interaction::isSurfaceInteraction() {
 
 SurfaceInteraction::SurfaceInteraction(Vector4 position, Vector4 positionError, Vector2 uv, Vector4 direction, Vector4 dpdu, Vector4 dpdv, Vector4 dndu, Vector4 dndv, float time, Shape* shape) {
   this->position = position;
+  this->normal = normalize(cross(dpdu, dpdv));
+  
   this->positionError = positionError;
 
   this->uv = uv;
