84cc6e9f3691a2b105daf9b9829bbfc4f7fb79e4
William Lew <WilliamMilesLew@gmail.com>
2020-04-11 14:36:46 -0700

    added: InteractionShading structure

--- src/interaction.cpp
+++ src/interaction.cpp
@@ -7,7 +7,7 @@ bool Interaction::isSurfaceInteraction() {
 SurfaceInteraction::SurfaceInteraction(Vector4 position, Vector4 positionError, Vector2 uv, Vector4 direction, Vector4 dpdu, Vector4 dpdv, Vector4 dndu, Vector4 dndv, float time, Shape* shape) {
   this->position = position;
   this->normal = normalize(cross(dpdu, dpdv));
-  
+
   this->positionError = positionError;
 
   this->uv = uv;
@@ -20,4 +20,10 @@ SurfaceInteraction::SurfaceInteraction(Vector4 position, Vector4 positionError, 
 
   this->time = time;
   this->shape = shape;
+
+  this->shading.normal = this->normal;
+  this->shading.dpdu = this->dpdu;
+  this->shading.dpdv = this->dpdv;
+  this->shading.dndu = this->dndu;
+  this->shading.dndv = this->dndv;
 }
--- src/interaction.h
+++ src/interaction.h
@@ -13,6 +13,12 @@ struct Interaction {
   bool isSurfaceInteraction();
 };
 
+struct InteractionShading {
+  Vector4 normal;
+  Vector4 dpdu, dpdv;
+  Vector4 dndu, dndv;
+};
+
 class SurfaceInteraction : public Interaction {
 public:
   Vector2 uv;
@@ -20,6 +26,7 @@ public:
   Vector4 dndu, dndv;
 
   Shape* shape;
+  InteractionShading shading;
 
   SurfaceInteraction(Vector4 position, Vector4 positionError, Vector2 uv, Vector4 direction, Vector4 dpdu, Vector4 dpdv, Vector4 dndu, Vector4 dndv, float time, Shape* shape);
 };
