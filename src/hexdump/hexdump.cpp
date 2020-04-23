7a5f1b1575d240b7444133765c46d9da4f3a64fa
William Lew <WilliamMilesLew@gmail.com>
2020-04-23 00:16:01 -0700

    added: spacing option for hexdump

--- src/hexdump/hexdump.cpp
+++ src/hexdump/hexdump.cpp
@@ -7,8 +7,14 @@
 int main(int argn, const char** argv) {
   uint64_t blockSize = 512;
 
-  if (access(argv[1], F_OK) == 0) {
-    FILE* file = fopen(argv[1], "rb");
+  const char* fileName = argv[1];
+  int characterSpacing = 2;
+  if (argn == 3) {
+    characterSpacing = std::atoi(argv[2]);
+  }
+
+  if (access(fileName, F_OK) == 0) {
+    FILE* file = fopen(fileName, "rb");
 
     fseek(file, 0, SEEK_END);
     uint64_t fileSize = ftell(file);
@@ -17,7 +23,7 @@ int main(int argn, const char** argv) {
     uint64_t bytesPerLine = 32;
     uint64_t numberOfLines = fileSize / bytesPerLine;
 
-    printf("Hexdump for %s with %ld blocks:\n\n", argv[1], fileSize / blockSize);
+    printf("Hexdump for %s with %ld blocks:\n\n", fileName, fileSize / blockSize);
 
     char* buffer = (char*)malloc(bytesPerLine);
     for (int x = 0; x < numberOfLines; x++) {
@@ -28,11 +34,17 @@ int main(int argn, const char** argv) {
       fread(buffer, 1, bytesPerLine, file);
       printf("%06X: ", (unsigned int)(x * bytesPerLine));
       for (int y = 0; y < bytesPerLine; y++) {
-        if (y != 0 && y % 8 == 0) {
-          printf(" ");
+        if (y != 0) {
+          if (y % 8 == 0) {
+            printf(" ");
+          }
+
+          if (y % characterSpacing == 0) {
+            printf(" ");
+          }
         }
 
-        printf("%02X ", buffer[y] & 0xff);
+        printf("%02X", buffer[y] & 0xff);
       }
 
       printf(" | ");
