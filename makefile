1c2f609c886bafdce15dd84274fa78c4392e5a48
William Lew <WilliamMilesLew@gmail.com>
2020-04-24 14:01:30 -0700

    added: printout for shell interaction

--- makefile
+++ makefile
@@ -13,7 +13,7 @@ NVCC=$(CUDA_PATH)/bin/nvcc
 CUDA_FLAGS=--gpu-architecture=sm_30
 
 EXEC=cuda-pbrt.out
-EXEC_ARGS=--help
+EXEC_ARGS=--interactive
 
 HEXDUMP_EXEC=hexdump.out
 HEXDUMP_EXEC_ARGS=dump/drive
--- src/shell.cpp
+++ src/shell.cpp
@@ -13,22 +13,14 @@ Shell::~Shell() {
 }
 
 void Shell::parseArguments() {
-  mode = ShellMode::RunOnce;
+  mode = ShellMode::Help;
 
   if (std::find(arguments.begin(), arguments.end(), "--interactive") != arguments.end()) {
     mode = ShellMode::Interactive;
   }
 
-  if (std::find(arguments.begin(), arguments.end(), "--help") != arguments.end()) {
-    mode = ShellMode::Help;
-  }
-
-  if (mode == ShellMode::Interactive) {
-    interactive();
-  }
-
-  if (mode == ShellMode::RunOnce) {
-    
+  if (std::find(arguments.begin(), arguments.end(), "--input") != arguments.end()) {
+    mode = ShellMode::RunOnce;
   }
 
   if (mode == ShellMode::Help) {
@@ -37,9 +29,28 @@ void Shell::parseArguments() {
     printf("\n");
     printf("  --help                 display help description\n");
     printf("  --interactive          start program in interactive mode\n");
+    printf("  --input <file>         render scene with provided scene file\n");
+  }
+
+  if (mode == ShellMode::Interactive) {
+    interactive();
+  }
+
+  if (mode == ShellMode::RunOnce) {
+
   }
 }
 
 void Shell::interactive() {
-
+  printf("  ██████╗██╗   ██╗██████╗  █████╗     ██████╗ ██████╗ ██████╗ ████████╗ \n");
+  printf(" ██╔════╝██║   ██║██╔══██╗██╔══██╗    ██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝ \n");
+  printf(" ██║     ██║   ██║██║  ██║███████║    ██████╔╝██████╔╝██████╔╝   ██║    \n");
+  printf(" ██║     ██║   ██║██║  ██║██╔══██║    ██╔═══╝ ██╔══██╗██╔══██╗   ██║    \n");
+  printf(" ╚██████╗╚██████╔╝██████╔╝██║  ██║    ██║     ██████╔╝██║  ██║   ██║    \n");
+  printf("  ╚═════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝    ╚═╝     ╚═════╝ ╚═╝  ╚═╝   ╚═╝    \n");
+  printf("\n");
+  printf("  1 - open file system\n");
+  printf("  2 - parse scene file\n");
+  printf("  9 - exit program\n");
+  printf("\n");
 }
